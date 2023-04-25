576c7ed0dfb7025adfcc877f5df738a72759820c
Radoslav Mráz <radoslav.mraz95@gmail.com>
2023-04-25 14:59:44 +0200

    corrected embedsegmodel definition

--- embed_seg.py
+++ embed_seg.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
@@ -5,12 +6,11 @@ from erfnet.erfnet import Encoder, Decoder
 
 
 class EmbedSegModel(nn.Module):
-    def __init__(self, num_classses: int, sigma_2d: bool=True):
+    def __init__(self, sigma_2d: bool=False):
         super().__init__()
         self.encoder = Encoder()
 
-        self.seed_branch = Decoder(num_classses)
-        self.seed_activation = F.softmax if num_classses > 2 else F.sigmoid
+        self.seed_branch = Decoder(1)
         self.instance_branch = Decoder(3 + int(sigma_2d))
     
     def forward(self, x):
@@ -20,4 +20,4 @@ class EmbedSegModel(nn.Module):
 
         offset_map = instance_branch[:, :2, :, :]
 
-        return self.seed_activation(self.seed_branch(z)), F.tanh(offset_map), F.elu(instance_branch[:, 2:, :, :])
+        return F.sigmoid(self.seed_branch(z)), F.tanh(offset_map), torch.exp(instance_branch[:, 2:, :, :])
