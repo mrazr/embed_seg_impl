0fcea18af22d07d6d09a51b00c52aa6f871290e8
Radoslav Mráz <radoslav.mraz95@gmail.com>
2023-04-25 14:58:12 +0200

    corrected the loss function implementation

--- utils/loss_functions.py
+++ utils/loss_functions.py
@@ -8,8 +8,7 @@ def phi_k_ei(ei, Ck, sigma):
     return torch.exp(-torch.square(torch.norm(ei - Ck)) / (2 * (sigma * sigma)))
 
 
-def loss_function_per_on_sample(pred: torch.Tensor, medoids_map: torch.Tensor, instance_map: torch.Tensor) -> torch.Tensor:
-    seed_map, offset_x_map, offset_y_map, sigma_map = pred[0], pred[1], pred[2], pred[3]
+def loss_function_per_on_sample(seed_map: torch.Tensor, offset_yx_map: torch.Tensor, sigma_map: torch.Tensor, medoids_map: torch.Tensor, instance_map: torch.Tensor, dev: str) -> torch.Tensor:
 
     instance_ids = torch.unique(instance_map, sorted=True)
 
@@ -17,27 +16,27 @@ def loss_function_per_on_sample(pred: torch.Tensor, medoids_map: torch.Tensor, i
     seed_loss = 0.0
     hinge_loss = 0.0
 
-    seed_map_true = torch.zeros_like(seed_map)
+    seed_map_true = torch.zeros_like(seed_map).to(dev)
 
     for k in instance_ids:
         if k == 0:
             continue
-        k_coords = torch.nonzero(instance_map[0] == k, as_tuple=True)
+        yy, xx = torch.nonzero(instance_map[0] == k, as_tuple=True)  # yy, xx
 
         # COMPUTE the SIGMA part of loss, we'll also need sigmas for the other loss functions
-        sigmas = sigma_map[k_coords]  # should be (n,) shape
+        sigmas = sigma_map[:, yy, xx]  # should be (n,) shape
         sigma_k = torch.mean(sigmas)
 
         sigma_loss += sigma_loss_fn(sigma_k, sigmas)
 
         hinge_margin = torch.sqrt(-LN05 * 2 * (sigma_k*sigma_k))
 
-        for y, x in zip(*k_coords):
+        for y, x in zip(yy, xx):
             Ck = medoids_map[:, y, x]  # (2, )
-            ei = torch.asarray([y + offset_y_map[y, x], x + offset_x_map[y, x]])
+            ei = torch.asarray([y + offset_yx_map[0, y, x], x + offset_yx_map[1, y, x]]).to(dev)
             prob = phi_k_ei(ei, Ck, sigma_k)
 
-            seed_map_true[y, x] = prob
+            seed_map_true[:, y, x] = prob
 
             # seed_loss += torch.square(seed_map[y, x] - prob)
 
@@ -47,10 +46,10 @@ def loss_function_per_on_sample(pred: torch.Tensor, medoids_map: torch.Tensor, i
     return hinge_loss + seed_loss + sigma_loss
 
 
-def embed_seg_loss_fn(batch_preds: torch.Tensor, batch_medoids_maps: torch.Tensor, batch_instance_maps: torch.Tensor):
+def embed_seg_loss_fn(seed_map_pred: torch.Tensor, offset_yx_map_pred: torch.Tensor, sigma_map_pred: torch.Tensor, batch_medoids_maps: torch.Tensor, batch_instance_maps: torch.Tensor, dev: str):
     loss = 0.0
-    for i in range(batch_preds.shape[0]):
-        loss += loss_function_per_on_sample(batch_preds[i], batch_medoids_maps[i], batch_instance_maps[i])
+    for i in range(seed_map_pred.shape[0]):
+        loss += loss_function_per_on_sample(seed_map_pred[i], offset_yx_map_pred[i], sigma_map_pred[i], batch_medoids_maps[i], batch_instance_maps[i], dev)
     return loss
 
 
