18c2023df6857754e875a2a8ec4aba4c709dc39e
Radoslav Mráz <radoslav.mraz95@gmail.com>
2023-04-26 14:38:45 +0200

    added post processing and visualization routines

---
+++ utils/post_processing.py
@@ -0,0 +1,70 @@
+import dataclasses
+import typing
+
+import numpy as np
+
+
+@dataclasses.dataclass
+class Cluster:
+    id: int
+    center: typing.Tuple[int, int]
+    pixels: np.ndarray
+    sigma: float
+
+
+@dataclasses.dataclass
+class Instance:
+    id: int
+    pixels: np.ndarray
+    cluster: Cluster
+
+
+def get_instances(seed_map: np.ndarray, offset_yx_map: np.ndarray, sigma_map: np.ndarray) -> typing.List[Instance]:
+    seeds_y, seeds_x = np.nonzero(seed_map > 0.5)
+    seed_values = seed_map[seeds_y, seeds_x]
+    seed_coords = np.stack((seeds_y, seeds_x), axis=1)
+    seed_offsets = offset_yx_map[: seeds_y, seeds_x]
+
+    seed_offsets[0] *= seed_map.shape[0]
+    seed_offsets[1] *= seed_map.shape[1]
+
+    seed_sigmas = sigma_map[seeds_y, seeds_x] * (seed_map.shape[0] // 2)
+
+    idx_sort = np.argsort(seed_values)[::-1]
+
+    seed_values_desc = seed_values[idx_sort]
+    seed_coords_desc = seed_coords[idx_sort]
+    seed_offsets_desc = seed_offsets[idx_sort]
+    seed_sigmas_desc = seed_sigmas[idx_sort]
+
+    seed_coords_desc_orig = seed_coords_desc.copy()
+    seed_coords_desc += seed_offsets_desc
+    seed_coords_desc = np.round(seed_coords_desc).astype(np.int32)
+
+    instances: typing.List[Instance] = []
+    next_instance_id = 1
+
+    while seed_coords_desc.shape[0] > 0:
+        seed_coord = seed_coords_desc[0]
+        seed_sigma = seed_sigmas_desc[0]
+        distances = np.linalg.norm(seed_coords_desc - seed_coord, axis=1)
+        member_indexes = np.nonzero(distances < seed_sigma)
+
+        member_coords = seed_coords_desc[member_indexes]
+
+        cluster = Cluster(next_instance_id, (seed_coord[0], seed_coord[1]), member_coords, seed_sigma)
+
+        instance = Instance(next_instance_id, seed_coords_desc_orig[member_indexes], cluster)
+        instances.append(instance)
+        next_instance_id += 1
+
+        indices = set(range(seed_coords_desc.shape[0]))
+        member_indexes = set(member_indexes)
+
+        new_indices = list(indices.difference(member_indexes))
+
+        seed_coords_desc = seed_coords_desc[new_indices]
+        seed_sigmas_desc = seed_sigmas_desc[new_indices]
+        seed_coords_desc_orig = seed_coords_desc_orig[new_indices]
+
+    return instances
---
+++ utils/visualize.py
@@ -0,0 +1,57 @@
+import typing
+
+import numpy as np
+from skimage import color, draw, util
+
+from post_processing import Cluster, Instance
+
+
+def visualize_pixel_offsets(offset_yx_map: np.ndarray, image: np.ndarray, alpha=0.5) -> typing.Tuple[np.ndarray, np.ndarray]:
+    angles = np.rad2deg(np.arctan2(offset_yx_map[0], offset_yx_map[1])) + 180
+    norms = np.linalg.norm(offset_yx_map, axis=0)
+    norms = norms / np.max(norms)
+
+    hsv = np.dstack((angles / 360.0, norms, np.ones_like(norms)))
+
+    rgb = color.hsv2rgb(hsv)
+
+    overlaid = util.img_as_float32(np.dstack((image,) * 3))
+
+    overlaid = alpha * rgb + (1.0 - alpha) * overlaid
+
+    return rgb, overlaid
+
+
+def visualize_clusters(clusters: typing.List[Cluster], image: np.ndarray) -> np.ndarray:
+    vis = np.zeros_like(image, dtype=np.uint32)
+
+    for cluster in clusters:
+        for coord in cluster.pixels:
+            if not (0 <= coord[0] < image.shape[0] and 0 <= coord[1] < image.shape[1]):
+                continue
+            coord = np.round(coord).astype(np.uint32)
+            rr, cc = draw.disk(tuple(coord), 3, shape=image.shape)
+            vis[rr, cc] = cluster.id
+
+    rgb_vis = np.round(255 * color.label2rgb(vis, image)).astype(np.uint8)
+
+    for cluster in clusters:
+        center = np.round(cluster.center).astype(np.uint8)
+        rr, cc = draw.disk(tuple(center), 5, shape=vis.shape)
+        rgb_vis[rr, cc] = [255, 255, 255]
+        rr, cc = draw.circle_perimeter_aa(center[0], center[1], cluster.sigma, shape=vis.shape)
+        rgb_vis[rr, cc] = [255, 255, 255]
+
+    return rgb_vis
+
+
+def visualize_instances(instances: typing.List[Instance], image: np.ndarray) -> np.ndarray:
+    vis = np.zeros_like(image, dtype=np.uint32)
+
+    for instance in instances:
+        for pixel in instance.pixels:
+            vis[pixel[0], pixel[1]] = instance.id
+
+    rgb = np.round(255 * color.label2rgb(vis, image)).astype(np.uint8)
+
+    return rgb
