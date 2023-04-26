reorganization + fixed the loss function computation

loss_functions.py
@@ -25,11 +25,13 @@ def loss_function_per_on_sample(seed_map: torch.Tensor, offset_yx_map: torch.Ten
     ei_yx_map = grid + offset_yx_map  # (2, 192, 192)
 
     mean_sigma_map = torch.zeros_like(sigma_map).to(dev)  # (1, 192, 192)
-    hinge_margin_map = torch.zeros_like(sigma_map).to(dev)  # (1, 192, 192)
+    # hinge_margin_map = torch.zeros_like(sigma_map).to(dev)  # (1, 192, 192)
 
     # phi_k_ei_map = torch.linalg.norm(ei_yx_map - medoids_map)
 
     smooth_loss = torch.tensor(0.0).to(dev)
+
+    hinge_loss = 0.0
 
     for k in instance_ids:
         if k == 0:
@@ -48,7 +50,11 @@ def loss_function_per_on_sample(seed_map: torch.Tensor, offset_yx_map: torch.Ten
 
         hinge_margin = torch.sqrt(-LN05 * 2 * (sigma_k*sigma_k))
 
-        hinge_margin_map[:, yy, xx] = hinge_margin
+        ei_s = ei_yx_map[:, yy, xx]
+        centers = medoids_map[:, yy, xx]
+        hinge_loss += torch.sum(torch.maximum(torch.linalg.norm(ei_s - centers, dim=0) - hinge_margin, torch.tensor(0.0).to(dev)))
+
+        # hinge_margin_map[:, yy, xx] = hinge_margin
         # for y, x in zip(yy, xx):
         #     Ck = medoids_map[:, y, x]  # (2, )
         #     ei = torch.asarray([y + offset_yx_map[0, y, x], x + offset_yx_map[1, y, x]]).to(dev)
@@ -62,7 +68,7 @@ def loss_function_per_on_sample(seed_map: torch.Tensor, offset_yx_map: torch.Ten
 
     phi_k_ei_map = torch.exp(-torch.square(torch.linalg.norm(ei_yx_map - medoids_map, dim=0)) / (2 * torch.square(mean_sigma_map)))
 
-    hinge_loss = torch.sum(torch.maximum(torch.linalg.norm(ei_yx_map - medoids_map, dim=0) - hinge_margin_map, torch.tensor(0).to(dev)))
+    # hinge_loss = torch.sum(torch.maximum(torch.linalg.norm(ei_yx_map - medoids_map, dim=0) - hinge_margin_map, torch.tensor(0).to(dev)))
 
     # sigma_loss = torch.sum(torch.abs(sigma_map - mean_sigma_map))
 

test_loss.py
@@ -1,7 +1,6 @@
 import torch
 
-from utils import loss_functions
-
+import loss_functions
 
 if __name__ == '__main__':
     medoids_true = torch.randint(20, 100, (2, 25, 25))

train.py
@@ -10,13 +10,14 @@ from omegaconf import DictConfig, OmegaConf
 import wandb
 from torch import utils
 from tqdm import tqdm
-from torchvision.utils import make_grid
 
 import torch.optim as optim
 
 import embed_seg
 import visualize
-from utils import image_dataset, loss_functions, post_processing
+from data_processing.utils import image_dataset
+import loss_functions
+import post_processing
 
 
 @hydra.main(config_path='experiments', config_name='config.yaml')
@@ -122,13 +123,24 @@ def train(cfg: DictConfig):
                 cluster_vis = visualize.visualize_clusters([instance.cluster for instance in instances], img)
                 instance_vis = visualize.visualize_instances(instances, img)
 
-                fig, axs = plt.subplots(1, 6, figsize=(20, 8))
+                fig, axs = plt.subplots(1, 6, figsize=(30, 12))
                 axs[0].imshow(img)
+                axs[0].set_title('image')
+
                 axs[1].imshow(seed_map)
+                axs[1].set_title('seediness')
+
                 axs[2].imshow(sigma_map)
+                axs[2].set_title('sigmas')
+
                 axs[3].imshow(offset_vis_overlay)
+                axs[3].set_title('offset vis')
+
                 axs[4].imshow(cluster_vis)
+                axs[4].set_title('cluster')
+
                 axs[5].imshow(instance_vis)
+                axs[5].set_title('instances')
 
                 fig.tight_layout(pad=0)
                 fig.canvas.draw()

visualize.py
@@ -3,7 +3,7 @@ import typing
 import numpy as np
 from skimage import color, draw, util
 
-from utils.post_processing import Cluster, Instance
+from post_processing import Cluster, Instance
 
 
 def visualize_pixel_offsets(offset_yx_map: np.ndarray, image: np.ndarray, alpha=0.5) -> typing.Tuple[np.ndarray, np.ndarray]: