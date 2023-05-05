55adeadf11d7467dcd262b1b58f31fef13d99d7b
Radoslav Mráz <radoslav.mraz95@gmail.com>
2023-05-05 15:20:53 +0200

    cleaned the code in `loss_functions.py`

--- loss_functions.py
+++ loss_functions.py
@@ -1,25 +1,25 @@
+import typing
+
 import torch
 
 from lovasz_losses import lovasz_hinge
 
 
-LN05 = torch.tensor(-0.69314718056)
-
-
-def phi_k_ei(ei, Ck, sigma):
-    return torch.exp(-torch.square(torch.norm(ei - Ck)) / (2 * (sigma * sigma)))
-
-
-def new_loss(seed_map: torch.Tensor, offset_yx_map: torch.Tensor, sigma_map: torch.Tensor, centers_map: torch.Tensor, instance_map: torch.Tensor, dev: str) -> torch.Tensor:
+def compute_losses_per_sample(seed_map: torch.Tensor, offset_yx_map: torch.Tensor, sigma_map: torch.Tensor, centers_map: torch.Tensor, instance_map: torch.Tensor, dev: str) -> \
+        typing.Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
     """
+    Computes the loss for one sample and returns the three constituent losses.
 
-    :param seed_map: (1, H, W) torch.Tensor
+    :param seed_map: (1, H, W) torch.Tensor [-1, 1]
     :param offset_yx_map: (2, H, W) torch.Tensor [-1, 1]
-    :param sigma_map: (1, H, W) torch.Tensor
-    :param centers_map: (3, H, W) torch.Tensor
+    :param sigma_map: (2, H, W) torch.Tensor [0, 1]
+    :param centers_map: (1, H, W) torch.Tensor of single pixels, where the value of each non-zero pixel corresponds to
+        the id of instance that the pixel is the center of. So for each instance in `instance_map`, there is only one
+        single pixel in `centers_map` that is located in the center (approximate medoid) and bears the id of that
+        instance.
     :param instance_map: (H, W) torch.Tensor
-    :param dev: str
-    :return:
+    :param dev: str device identifier
+    :return: tuple of 3 losses: `l_instance`, `l_seed`, `l_var` loss as detailed in the paper
     """
 
     instance_map = torch.squeeze(instance_map)
@@ -29,29 +29,20 @@ def new_loss(seed_map: torch.Tensor, offset_yx_map: torch.Tensor, sigma_map: tor
                                       torch.linspace(0.0, 1.0, seed_map.shape[1]), indexing='ij')
     pixel_grid = torch.permute(torch.dstack((mesh_yy, mesh_xx)), dims=(2, 0, 1)).to(dev)
     
-    # print(f'offset y mean = {torch.mean(offset_yx_map[0])}\toffset x mean = {torch.mean(offset_yx_map[1])}')
-    
     shifted_pixel_grid = pixel_grid + offset_yx_map
 
     l_var = 0.0
     l_instance = 0.0
     l_seed = 0.0
 
-    # print(f'centers shape is {centers_map.shape}')
-
     for k in instance_ids:
         if k == 0:
             continue
-        # instance_center = torch.unsqueeze(torch.unsqueeze(torch.argwhere(centers_map[0] == k)[0], dim=-1), dim=-1)
         instance_yy, instance_xx = torch.nonzero(instance_map == k, as_tuple=True)
 
-        # instance_center = torch.unsqueeze(torch.unsqueeze(centers_map[:, instance_yy[0], instance_xx[0]], dim=-1), dim=-1)  # (2, 1, 1)
         instance_center = torch.unsqueeze(torch.unsqueeze(torch.argwhere(centers_map == k)[0], dim=-1), dim=-1).to(torch.float32)
         instance_center[0] /= centers_map.shape[0]
         instance_center[1] /= centers_map.shape[1]
-        
-        # print(f'instance center is {instance_center.cpu()}')
-        # print(f'instance center shape = {instance_center.shape}')
 
         instance_sigmas = sigma_map[:, instance_yy, instance_xx] # (2, n)
 
@@ -59,20 +50,8 @@ def new_loss(seed_map: torch.Tensor, offset_yx_map: torch.Tensor, sigma_map: tor
         scaled_mean_instance_sigma = torch.exp(-10 * mean_instance_sigma) # (2, 1)
 
         l_var += torch.mean(torch.pow(instance_sigmas - mean_instance_sigma, 2))
-        # print(f'instance sigmas shape = {instance_sigmas.shape}')
-        # print(f'mean instance sigma shape = {mean_instance_sigma.shape}')
-        # print(f'shape of shift px gr [0, :, :] = {shifted_pixel_grid[0, :, :].shape}')
-        # print(f'shape of shift px gr [0] = {shifted_pixel_grid[0].shape}')
-        # print(f'shape of the big - {torch.square(shifted_pixel_grid[0, :, :] - instance_center[0, :, :]).shape}')
-
-        # D_k = torch.exp(-1.0 * (torch.square(torch.linalg.norm(shifted_pixel_grid - instance_center, dim=0))) / (2 * mean_instance_sigma * mean_instance_sigma + 1e-12))
-        D_k = torch.exp(-1.0 * torch.square(shifted_pixel_grid[0, :, :] - instance_center[0, :, :]) / scaled_mean_instance_sigma[0, 0] - 
+        D_k = torch.exp(-1.0 * torch.square(shifted_pixel_grid[0, :, :] - instance_center[0, :, :]) / scaled_mean_instance_sigma[0, 0] -
                         torch.square(shifted_pixel_grid[1, :,:] - instance_center[1, :, :]) / scaled_mean_instance_sigma[1, 0])
-        # D_k = torch.exp(-1.0 * torch.square(shifted_pixel_grid[0, :, :] - instance_center[0, :, :]) / scaled_mean_instance_sigma[0, 0] - 
-        #                 torch.square(shifted_pixel_grid[1, :,:] - instance_center[1, :, :]) / scaled_mean_instance_sigma[1, 0])
-        # D_k = torch.exp(-1.0 * torch.square(shifted_pixel_grid[0, :,:] - instance_center[0, :, :]) / scaled_mean_instance_sigma[0, 0])
-        
-        # print(f'd_k shape = {D_k.shape}')
 
         B_k = torch.where(instance_map == k, 1.0, 0.0)
         l_instance += lovasz_hinge(2.0 * torch.unsqueeze(D_k, dim=0) - 1.0, torch.unsqueeze(B_k, dim=0))
@@ -88,19 +67,12 @@ def new_loss(seed_map: torch.Tensor, offset_yx_map: torch.Tensor, sigma_map: tor
 
 
 def embed_seg_loss_fn(seed_map_pred: torch.Tensor, offset_yx_map_pred: torch.Tensor, sigma_map_pred: torch.Tensor, batch_medoids_maps: torch.Tensor, batch_instance_maps: torch.Tensor, dev: str):
-    # loss = 0.0
     L_instance = 0.0
     L_seed = 0.0
     L_var = 0.0
     for i in range(seed_map_pred.shape[0]):
-        # losses = loss_function_per_on_sample(seed_map_pred[i], offset_yx_map_pred[i], sigma_map_pred[i], batch_medoids_maps[i], batch_instance_maps[i], dev)
-        losses = new_loss(seed_map_pred[i], offset_yx_map_pred[i], sigma_map_pred[i], batch_medoids_maps[i], batch_instance_maps[i], dev)
+        losses = compute_losses_per_sample(seed_map_pred[i], offset_yx_map_pred[i], sigma_map_pred[i], batch_medoids_maps[i], batch_instance_maps[i], dev)
         L_instance += losses[0]
         L_seed += losses[1]
         L_var += losses[2]
     return L_instance, L_seed, 10 * L_var
-
-
-def sigma_loss_fn(sigma_k, sigmas_k):
-    return torch.sum(torch.abs(sigmas_k - sigma_k))
-
